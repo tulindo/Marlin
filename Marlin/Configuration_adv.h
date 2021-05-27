453db44fc61e50912d2d77ab90c00694f011be63
Paolo Tuninettom <paolo.tuninetto@gmail.com>
2021-05-27 13:01:24 +0200

    Aumentato bed_temp_period

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -220,7 +220,7 @@
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
-  #define WATCH_BED_TEMP_PERIOD                20 // Seconds
+  #define WATCH_BED_TEMP_PERIOD                30 // Seconds
   #define WATCH_BED_TEMP_INCREASE               4 // Degrees Celsius
 #endif
 
